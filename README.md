af2b8752b324848e68607ee2b6d14d395bc0a66c
Hasan Bayat <hasanbayat1393@gmail.com>
2021-09-28 22:58:18 +0330

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# hasan.bayat.io
+
+My personal website source.
+
+Built with [@lekoarts/gatsby-theme-cara](https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-cara)
---
+++ gatsby-config.js
@@ -0,0 +1,72 @@
+require(`dotenv`).config()
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const googleAnalyticsTrackingId = process.env.GOOGLE_ANALYTICS_ID
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: "Hasan Bayat | Personal Website",
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
+    siteTitleAlt: `Hasan Bayat | Personal Website`,
+    siteHeadline: "Hasan Bayat's Personal Website",
+    siteUrl: "https://hasan.bayat.io",
+    // Used for SEO
+    siteDescription: `Creator, Influencer, Passionate`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@hasanbayat_me`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    googleAnalyticsTrackingId && {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Hasan Bayat | Personal Website`,
+        short_name: `Hasan Bayat`,
+        description: `Creator, Influencer, Passionate`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+}
